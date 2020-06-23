efae927fedb5ce012b91a936c10f2fec32768320
Matheus Moreira <matheustjp111@gmail.com>
2020-06-23 15:10:43 -0300

    Módulo Back-end com Node.Js finalizado.

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,91 @@
 const express = require('express');
+const { uuid, isUuid } = require('uuidv4');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  return res.json({ message: 'Hello World.' });
+app.use(express.json());
+
+const projects = [];
+
+function logRequests(req, res, next) {
+  const { method, url } = req;
+
+  const logLabel = `[${method.toUpperCase()}] ${url}`;
+
+  console.time(logLabel);
+
+  return next();
+
+  console.timeEnd(logLabel);
+}
+
+function validateProjectId(req, res, next) {
+  const { id } = req.params;
+
+  if (!isUuid(id)) {
+    return res.status(400).json({ error: 'Invalid project ID.' });
+  }
+
+  return next();
+}
+
+app.use(logRequests);
+app.use('/projects/:id', validateProjectId);
+
+app.get('/projects', (req, res) => {
+  const { title } = req.query;
+
+  const results = title 
+    ? projects.filter(project => project.title.includes(title))
+    : projects;
+
+  return res.json(results);
+});
+
+app.post('/projects', (req, res) => {
+  const { title, owner } = req.body;
+
+  const project = { id: uuid(), title, owner };
+
+  projects.push(project);
+
+  return res.json(project);
+});
+
+app.put('/projects/:id', (req, res) => {
+  const { id } = req.params;
+  const { title, owner } = req.body;
+
+  const findIndex = projects.findIndex(project => project.id === id);
+
+  if( findIndex < 0 ){
+    return res.status(400).json({ error: 'Project not found.' });
+  }
+
+  const project = {
+    id,
+    title,
+    owner,
+  };
+
+  projects[findIndex] = project;
+
+  return res.json(project);
+});
+
+app.delete('/projects/:id', (req, res) => {
+  const { id } = req.params;
+  const { title, owner } = req.body;
+
+  const findIndex = projects.findIndex(project => project.id === id);
+
+  if( findIndex < 0 ){
+    return res.status(400).json({ error: 'Project not found.' });
+  }
+
+  projects.splice(findIndex, 1);
+
+  return res.status(204).send();
 });
 
 app.listen(3333, () => {
